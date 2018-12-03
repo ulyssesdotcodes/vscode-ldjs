ba2eec6454d49be9e0b949faeefa147857e7c614
Ulysses Popple <ulysses.popple@gmail.com>
2018-12-02 19:55:11 -0500

    More visuals

--- samples/libs/visuals.ldjs.js
+++ samples/libs/visuals.ldjs.js
@@ -1,3 +1,10 @@
+function zip(arrs) {
+    let shortest = arrs.reduce((acc, p) => acc.length < p.length ? acc : p)
+    return shortest.map(function(_, i){
+        return arrs.map(function(array){ return array[i]})
+    })
+}
+
 const visuals = (c) => ({
     ain: (g) => c.chope("audiodevicein")
         .connect(c.chop("resample", {"timeslice": c.tp(false), "method": c.mp(0), "relative": c.mp(0), "end": c.fp(0.03)}))
@@ -20,12 +27,36 @@ const visuals = (c) => ({
     bandvc: (b) => c.chan(c.ip(0), visuals(c).bandv(b)),
     mchan: (chan) => c.chan(c.sp(chan), c.chope("midiinmap")),
     mchop: (chan) => c.chope("midiinmap").connect(c.chop("select", {"channames": c.sp(chan)})),
-    frag: (fragname, uniforms) => c.top("glslmulti", {
-            "resolution": c.whp(c.np(1920), c.np(1080)),
+    frag: (fragname, uniforms) => c.top("glslmulti", Object.assign({
+            "resolutionw": c.ip(1920), 
+            "resolutionh": c.ip(1080),
             "pixeldat": c.datp(c.dat("text", {"file" : c.sp("scripts/Visuals/" + fragname)})),
             "outputresolution": c.mp(9),
-            "format": c.mp(4)
-        })
+            "format": c.mp(4) 
+        }, zip([Array(8).fill("uniname"), Array.from(Array(8).keys()), Object.keys(uniforms)]).reduce((acc, v) => { acc[v[0]+v[1]] = c.sp(v[2]); return acc; }, {})
+        , zip([Array(8).fill("value"), Array.from(Array(8).keys()), Object.values(uniforms)]).reduce((acc, v) => { acc[v[0]+v[1]] = v[2]; return acc; }, {}))),
+    adata: (v) => visuals(c).atex().connect(visuals(c).frag('audio_data.frag', {'i_volume': c.x4p(v)})),
+    noiset: (t) => c.top("noise", {"t": c.xyzp(c.fp(0), c.fp(0), t)}),
+    lines: (spacing, width) => visuals(c).frag("lines.frag", {"i_spacing": c.x4p(spacing), "i_width": c.x4p(width)}),
+    shapes: (size, width, sides) => visuals(c).frag("shapes.frag", {"i_size": c.x4p(size), "i_width": c.x4p(width), "i_sides": c.x4p(sides)}),
+    stringtheory: (time, angle, angle_delta, xoffset) =>
+        visuals(c).frag("string_theory.frag", 
+            { 
+                "i_time": c.x4p(time), 
+                "i_angle": c.x4p(angle), 
+                "i_angle_delta": c.x4p(angle_delta === undefined ? c.fp(0.4) : angle_delta), 
+                "i_xoff": c.x4p(xoffset === undefined ? c.fp(0) : xoffset)
+            }),
+    commandCode: (text) => c.top("text", {
+        "resolutionw": c.ip(1920), 
+        "resolutionh": c.ip(1080), 
+        "fontsizey": c.fp(16), 
+        "alignx": c.mp(0),
+        "aligny": c.mp(0),
+        "text": c.sp(text)
+    })
+
+
 })
 //export const rect = (c) => c.tope("rectangle")
 module.exports = visuals
--- samples/scratch.ldjs.js
+++ samples/scratch.ldjs.js
@@ -1,3 +1,25 @@
 let visuals = require('libs/visuals.ldjs.js');
 
-return c.top("rectangle", {"size": c.xyp(visuals(c).mchan("s1a"), c.fp(0.2))}).connect(c.tope('out')).out();
+// let lines = (spacing, width) => visuals(c).frag("lines.frag", {"i_spacing": c.x4p(spacing), "i_width": c.x4p(width)})
+
+// let shapes = (size, width, sides) => visuals(c).frag("shapes.frag", {"i_size": c.x4p(size), "i_width": c.x4p(width), "i_sides": c.x4p(sides)})
+
+// let stringtheory = (time, angle, angle_delta, xoffset) =>
+//     visuals(c).frag("string_theory.frag", 
+//         { 
+//             "i_time": c.x4p(time), 
+//             "i_angle": c.x4p(angle), 
+//             "i_angle_delta": c.x4p(angle_delta === undefined ? c.fp(0.4) : angle_delta), 
+//             "i_xoff": c.x4p(xoffset === undefined ? c.fp(0) : xoffset)
+//         })
+
+let commandCode = (text) => c.top("text", {
+    "resolutionw": c.ip(1920), 
+    "resolutionh": c.ip(1080), 
+    "fontsizey": c.fp(16), 
+    "alignx": c.mp(0),
+    "aligny": c.mp(0),
+    "text": c.sp(text)
+})
+
+return n({CommandCode}).out()
